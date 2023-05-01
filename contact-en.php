7c5150db04ae6906e088bbcd0cb266d3dd8a915e
sohaleKing <sohale_r@yahoo.com>
2023-05-01 09:59:28 -0400

    contact

--- contact-en.php
+++ contact-en.php
@@ -2,15 +2,15 @@
 if(isset($_POST['email'])) {
  
     // EDIT THE 2 LINES BELOW AS REQUIRED
-    $email_to = "info@atlastaxi.qc.ca";
-    $email_subject = "user contact by atlas website";
+    $email_to = "sohale@atlastaxi.qc.ca";
+    $email_subject = "Your email subject line";
  
     function died($error) {
         // your error code can go here
-        echo "Nous sommes désolés, mais des erreurs ont été trouvées dans le formulaire que vous avez soumis.";
-        echo "Ces erreurs apparaissent ci-dessous.<br /><br />";
+        echo "We are very sorry, but there were error(s) found with the form you submitted. ";
+        echo "These errors appear below.<br /><br />";
         echo $error."<br /><br />";
-        echo "Veuillez revenir en arrière et corriger ces erreurs.<br /><br />";
+        echo "Please go back and fix these errors.<br /><br />";
         die();
     }
  
@@ -19,7 +19,7 @@ if(isset($_POST['email'])) {
     if(!isset($_POST['name']) ||
         !isset($_POST['email']) ||
         !isset($_POST['message'])) {
-        died('Nous sommes désolés, mais des erreurs ont été trouvées dans le formulaire que vous avez soumis.');       
+        died('We are sorry, but there appears to be a problem with the form you submitted.');       
     }
  
      
@@ -32,24 +32,24 @@ if(isset($_POST['email'])) {
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
  
   if(!preg_match($email_exp,$email_from)) {
-    $error_message .= 'Ladresse e-mail que vous avez saisie ne semble pas valide.<br />';
+    $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
  
     $string_exp = "/^[A-Za-z .'-]+$/";
  
   if(!preg_match($string_exp,$name)) {
-    $error_message .= 'Le prénom que vous avez saisi ne semble pas valide.<br />';
+    $error_message .= 'The First Name you entered does not appear to be valid.<br />';
   }
  
   if(strlen($comments) < 2) {
-    $error_message .= 'Les commentaires que vous avez saisis ne semblent pas valides.<br />';
+    $error_message .= 'The Comments you entered do not appear to be valid.<br />';
   }
  
   if(strlen($error_message) > 0) {
     died($error_message);
   }
  
-    $email_message = "Détails du formulaire ci-dessous.\n\n";
+    $email_message = "Form details below.\n\n";
  
      
     function clean_string($string) {
@@ -72,7 +72,7 @@ $headers = 'From: '.$email_from."\r\n".
  
 <!-- include your own success html here -->
  
-Merci de nous contacter. Nous vous contacterons très bientôt.
+Thank you for contacting us. We will be in touch with you very soon.
  
 <?php
  
